a1acf5b8d7515f360dfb803d16b274a2e90cc615
TAMIL MANI.E <91011859+TamilmaniE@users.noreply.github.com>
2025-05-31 17:26:22 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/todo-app'),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     TodoModule,
   ],
 })
