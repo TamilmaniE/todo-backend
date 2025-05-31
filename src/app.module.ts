c8dc0aaabdc97d8c3f79ca019aa176999d494c02
TAMIL MANI.E <91011859+TamilmaniE@users.noreply.github.com>
2025-05-31 17:30:13 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
-
-
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MongooseModule.forRoot(process.env.MONGODB_URI!),
     TodoModule,
   ],
 })
